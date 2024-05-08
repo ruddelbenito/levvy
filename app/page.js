47c1c3d73fe7812d743fab294277152e207171c5
Russel Benito <ruddel.benito@gmail.com>
2024-05-07 19:08:04 -0600

    Reversed matrix order

--- app/page.js
+++ app/page.js
@@ -7,34 +7,34 @@ export default function Home() {
   let matrix = [];
 
   // instantiate matrix
-  for (let w1ndex = 0; w1ndex < word1.length + 1; w1ndex++) {
+  for (let w2ndex = 0; w2ndex < word2.length + 1; w2ndex++) {
     matrix.push([]);
-    for (let w2ndex = 0; w2ndex < word2.length + 1; w2ndex++) {
-      matrix[w1ndex].push(0);
+    for (let w1ndex = 0; w1ndex < word1.length + 1; w1ndex++) {
+      matrix[w2ndex].push(0);
     }
   }
 
   // iterate through matrix
-  for (let w1ndex = 0; w1ndex < matrix.length; w1ndex++) {
-    for (let w2ndex = 0; w2ndex < matrix[w1ndex].length; w2ndex++) {
+  for (let w2ndex = 0; w2ndex < matrix.length; w2ndex++) {
+    for (let w1ndex = 0; w1ndex < matrix[w2ndex].length; w1ndex++) {
 
       // base case: if the current index is at 0, the levenshtein distance is the length of the other word
-      if (w1ndex === 0) {
-        matrix[w1ndex][w2ndex] = w2ndex;
-      }
       if (w2ndex === 0) {
-        matrix[w1ndex][w2ndex] = w1ndex;
+        matrix[w2ndex][w1ndex] = w1ndex;
+      }
+      if (w1ndex === 0) {
+        matrix[w2ndex][w1ndex] = w2ndex;
       }
 
-      if (w1ndex !== 0 && w2ndex !== 0) {
+      if (w2ndex !== 0 && w1ndex !== 0) {
         // determine levenshtein distance based on current progress in the matrix
         // if the current letter is the same
-        if (word1[w1ndex - 1] === word2[w2ndex - 1]) {
-          matrix[w1ndex][w2ndex] = matrix[w1ndex-1][w2ndex-1];
+        if (word2[w2ndex - 1] === word1[w1ndex - 1]) {
+          matrix[w2ndex][w1ndex] = matrix[w2ndex-1][w1ndex-1];
         }
         // if the current letter is different
         else {
-          matrix[w1ndex][w2ndex] = Math.min(matrix[w1ndex - 1][w2ndex], matrix[w1ndex][w2ndex-1], matrix[w1ndex-1][w2ndex-1]) + 1;
+          matrix[w2ndex][w1ndex] = Math.min(matrix[w2ndex - 1][w1ndex], matrix[w2ndex][w1ndex-1], matrix[w2ndex-1][w1ndex-1]) + 1;
         }
       }
     }
@@ -47,7 +47,7 @@ export default function Home() {
       <p>word 1: {word1}</p>
       <p>word 2: {word2}</p>
 
-
+      
     </main>
   );
 }
