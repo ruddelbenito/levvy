aea225643f4c840df30889ef47719b7feb47457a
Russel Benito <ruddel.benito@gmail.com>
2024-05-11 15:28:31 -0600

    Revert "Started display implementation"
    This reverts commit 37a5bb12f3010c918bb5602eb018398a7aaa27bf.

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
@@ -6,22 +7,10 @@ export default function Home() {
   let matrix = [];
 
   // instantiate matrix
-  for (let w2ndex = 0; w2ndex < word2.length + 2; w2ndex++) {
+  for (let w2ndex = 0; w2ndex < word2.length + 1; w2ndex++) {
     matrix.push([]);
-    for (let w1ndex = 0; w1ndex < word1.length + 2; w1ndex++) {
-      // leave grid space for blanks and letters
-      if (w2ndex === 0 && w1ndex === 0) {
-        matrix[w2ndex].push('');
-      }
-      else if (w2ndex === 0) {
-        matrix[w2ndex].push(word1[w1ndex]);
-      }
-      else if (w1ndex === 0) {
-        matrix[w2ndex].push(word2[w2ndex]);
-      }
-      else {
-        matrix[w2ndex].push(0);
-      }
+    for (let w1ndex = 0; w1ndex < word1.length + 1; w1ndex++) {
+      matrix[w2ndex].push(0);
     }
   }
 
@@ -30,18 +19,18 @@ export default function Home() {
     for (let w1ndex = 0; w1ndex < matrix[w2ndex].length; w1ndex++) {
 
       // base case: if the current index is at 0, the levenshtein distance is the length of the other word
-      if (w2ndex === 1) {
+      if (w2ndex === 0) {
         matrix[w2ndex][w1ndex] = w1ndex;
       }
-      if (w1ndex === 1) {
+      if (w1ndex === 0) {
         matrix[w2ndex][w1ndex] = w2ndex;
       }
 
-      if (w2ndex > 1 && w1ndex > 1) {
+      if (w2ndex !== 0 && w1ndex !== 0) {
         // determine levenshtein distance based on current progress in the matrix
         // if the current letter is the same
         if (word2[w2ndex - 1] === word1[w1ndex - 1]) {
-          matrix[w2ndex][w1ndex] = matrix[w2ndex - 1][w1ndex - 1];
+          matrix[w2ndex][w1ndex] = matrix[w2ndex-1][w1ndex-1];
         }
         // if the current letter is different
         else {
@@ -58,15 +47,7 @@ export default function Home() {
       <p>word 1: {word1}</p>
       <p>word 2: {word2}</p>
 
-      <table>
-        {matrix.map((column) => {
-          return <tr key={column}>
-            {column.map((cell) => {
-              return <td key={cell}>{cell}</td>
-            })}
-          </tr>
-        })}
-      </table>
+      
     </main>
   );
 }
